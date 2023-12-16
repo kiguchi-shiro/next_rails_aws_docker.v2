d605882cdb2002379f69efa7f5b2978a15e86fe0
white <rulu0qa00ok0zxcv@gmail.com>
2023-12-16 23:56:59 +0900

    rubocop_revise

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'health_check', to: 'health_check#index'
+      get "health_check", to: "health_check#index"
     end
   end
 end
